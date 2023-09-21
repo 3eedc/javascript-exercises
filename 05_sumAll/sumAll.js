03d3301749770c9f2b74d567e9e9122da65c452a
3eedc <logansevillaleung@gmail.com>
2023-09-21 14:00:19 -0400

    HIP HIP

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(low, high) {
+    let sum = 0;
+    if((low || high) < 0){
+        return "ERROR";
+    }
+    if(low>high)
+    {
+        let thirdvalue;
+        thirdvalue = high;
+        high = low;
+        low = thirdvalue;
+    }
+    if(typeof high !== 'number'){
+        return "ERROR"
+    }
+    for(let i = low; i<=high; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
