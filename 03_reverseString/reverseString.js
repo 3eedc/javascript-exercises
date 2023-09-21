def0720c359e9d6a5e3d6b2fdd0f0de459fb5121
3eedc <logansevillaleung@gmail.com>
2023-09-21 08:45:55 -0400

    I DID IT YIPPEE

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    str = str.split('');
+    str = str.reverse();
+    return str.join('');
 };
 
 // Do not edit below this line
