eb292b0e45511a4e3a741fafdf0c029098e62e1a
3eedc <logansevillaleung@gmail.com>
2023-10-17 09:06:57 -0400

    temprature dmeon

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(C) {
+  return Math.round((C - 32) * 5/9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(F) {
+  return Math.round((F * 9/5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
