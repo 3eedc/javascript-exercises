cb72f9a8400ed5cd0ef3cb5085e34ff78110d535
3eedc <logansevillaleung@gmail.com>
2023-10-21 18:47:06 -0400

    i used chatgpt to help me lmfao

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
-};
+const removePunctuation = (x) => {
+    return x.replace(/[^a-zA-Z0-9]/g, '');
+  };
+  
+  const reverse = function (x) {
+    y = removePunctuation(x).toLowerCase().split('');
+    z = y.reverse();
+    return z.join('');
+  }
+  
+  const palindromes = function (x) {
+    const cleanInput = removePunctuation(x).toLowerCase();
+    const reversed = reverse(cleanInput);
+    return cleanInput === reversed;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
